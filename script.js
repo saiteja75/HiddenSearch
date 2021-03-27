e6a9f3972e6e17980c7d06ec617605ff22af19d0
saiteja <saiteja.kasam75@gmail.com>
2021-03-27 16:39:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let openBtn = document.getElementById('open');
-let closeBtn = document.getElementById('close');
-let container = document.getElementsByClassName('container')
+let searchBtn = document.getElementsByTagName('button');
+let inputText = document.getElementsByTagName('input');
 
-openBtn.addEventListener("click", function(e) {
-    container[0].classList.add('show-nav');
-});
-
-closeBtn.addEventListener("click", function(e) {
-    container[0].classList.remove('show-nav');
+searchBtn[0].addEventListener("click", function(e) {
+    if (inputText[0].classList.contains('active')) {
+        inputText[0].classList.remove('active');
+    } else {
+        inputText[0].classList.add('active');
+    }
 })
